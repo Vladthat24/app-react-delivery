fcb8766d8fa75f2951c28e0fbc9f80445633e1ec
Vladthat24 <yosshimendieta94@gmail.com>
2023-08-17 22:11:42 -0500

    Agregar jwt

--- src/Presentation/views/register/Register.tsx
+++ src/Presentation/views/register/Register.tsx
@@ -1,103 +1,109 @@
-import React from 'react';
-import {
-    Text,
-    View,
-    Image,
-    ScrollView,
-} from "react-native";
-import RounderButton from '../../../Presentation/components/RounderButton';
-import useViewModel from './ViewModel';
+import React, { useEffect } from "react";
+import { Text, View, Image, ScrollView, ToastAndroid } from "react-native";
+import RounderButton from "../../../Presentation/components/RounderButton";
+import useViewModel from "./ViewModel";
 import CustomElementRegistry from "../../../Presentation/components/CustomTextInput";
-import styles from './Styles';
+import styles from "./Styles";
 
 export const RegisterScreen = () => {
+  const {
+    name,
+    lastname,
+    email,
+    phone,
+    password,
+    confirmPassword,
+    onChange,
+    register,
+    errorMessage,
+  } = useViewModel();
 
-    const { name, lastname, email, phone, password, confirmPassword, onChange, register } = useViewModel();
+  useEffect(() => {
+    if (errorMessage != "") {
+      ToastAndroid.show(errorMessage, ToastAndroid.LONG);
+    }
+  }, [errorMessage]);
 
-    return (
-        <View style={styles.container}>
-            <Image
-                style={styles.imageBackground}
-                source={require("../../../../assets/chef.jpg")}
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.imageBackground}
+        source={require("../../../../assets/chef.jpg")}
+      />
+      <View style={styles.logoContainer}>
+        <Image
+          style={styles.logoImage}
+          source={require("../../../../assets/user_image.png")}
+        />
+        <Text style={styles.logoText}>Seleccionar Imagen</Text>
+      </View>
+      <View style={styles.form}>
+        <ScrollView>
+          <Text style={styles.formText}>Registarse</Text>
+
+          <CustomElementRegistry
+            placerholder="Nombres"
+            keyboardType="default"
+            image={require("../../../../assets/user.png")}
+            property="name"
+            onChangeText={onChange}
+            value={name}
+          />
+
+          <CustomElementRegistry
+            placerholder="Apellidos"
+            keyboardType="default"
+            image={require("../../../../assets/my_user.png")}
+            property="lastname"
+            onChangeText={onChange}
+            value={lastname}
+          />
+          <CustomElementRegistry
+            placerholder="Cel/Telf"
+            keyboardType="numeric"
+            image={require("../../../../assets/phone.png")}
+            property="phone"
+            onChangeText={onChange}
+            value={phone}
+          />
+          <CustomElementRegistry
+            placerholder="Correo Eletronico"
+            keyboardType="email-address"
+            image={require("../../../../assets/email.png")}
+            property="email"
+            onChangeText={onChange}
+            value={email}
+          />
+          <CustomElementRegistry
+            placerholder="Contraseña"
+            keyboardType="default"
+            image={require("../../../../assets/password.png")}
+            property="password"
+            onChangeText={onChange}
+            value={password}
+            secureTextEntry={true}
+          />
+          <CustomElementRegistry
+            placerholder="Confirmar Contraseña"
+            keyboardType="default"
+            image={require("../../../../assets/confirm_password.png")}
+            property="confirmPassword"
+            onChangeText={onChange}
+            value={confirmPassword}
+            secureTextEntry={true}
+          />
+          <View style={{ marginTop: 30 }}>
+            <RounderButton
+              text="Confirmar"
+              onPress={() => {
+                register();
+              }}
             />
-            <View style={styles.logoContainer}>
-                <Image style={styles.logoImage} source={require("../../../../assets/user_image.png")} />
-                <Text style={styles.logoText}>Seleccionar Imagen</Text>
-            </View>
-            <View style={styles.form}>
-
-                <ScrollView>
-
-
-                    <Text style={styles.formText}>Registarse</Text>
-
-                    <CustomElementRegistry
-                        placerholder='Nombres'
-                        keyboardType='default'
-                        image={require("../../../../assets/user.png")}
-                        property='name'
-                        onChangeText={onChange}
-                        value={name}
-                    />
-
-                    <CustomElementRegistry
-                        placerholder='Apellidos'
-                        keyboardType='default'
-                        image={require("../../../../assets/my_user.png")}
-                        property='lastname'
-                        onChangeText={onChange}
-                        value={lastname}
-                    />
-                    <CustomElementRegistry
-                        placerholder='Cel/Telf'
-                        keyboardType='numeric'
-                        image={require("../../../../assets/phone.png")}
-                        property='phone'
-                        onChangeText={onChange}
-                        value={phone}
-                    />
-                    <CustomElementRegistry
-                        placerholder='Correo Eletronico'
-                        keyboardType='email-address'
-                        image={require("../../../../assets/email.png")}
-                        property='email'
-                        onChangeText={onChange}
-                        value={email}
-                    />
-                    <CustomElementRegistry
-                        placerholder='Contraseña'
-                        keyboardType='default'
-                        image={require('../../../../assets/password.png')}
-                        property='password'
-                        onChangeText={onChange}
-                        value={password}
-                        secureTextEntry={true}
-                    />
-                    <CustomElementRegistry
-                        placerholder='Confirmar Contraseña'
-                        keyboardType='default'
-                        image={require('../../../../assets/confirm_password.png')}
-                        property='confirmPassword'
-                        onChangeText={onChange}
-                        value={confirmPassword}
-                        secureTextEntry={true}
-                    />
-                    <View style={{ marginTop: 30 }}>
-                        {/*           <Button
-                    title="Enviar"
-                    onPress={() => ToastAndroid.show("Click", ToastAndroid.LONG)}
-                    color="orange"
-                  /> */}
-                        <RounderButton
-                            text="Confirmar"
-                            onPress={() => register()}
-                        />
-                    </View>
-                </ScrollView>
-            </View>
-        </View>
-    );
+          </View>
+        </ScrollView>
+      </View>
+    </View>
+  );
 };
-
 
 export default RegisterScreen;
--- src/Presentation/views/register/ViewModel.tsx
+++ src/Presentation/views/register/ViewModel.tsx
@@ -5,6 +5,8 @@ import { RegisterAuthUseCase } from '../../../Domain/useCases/auth/RegisterAuth'
 
 const RegisterViewModel=()=> {
 
+    const [errorMessage,setErrorMessage]=useState('');
+
     const [values,setValues]=useState({
         name:'',
         lastname:'',
@@ -19,14 +21,49 @@ const RegisterViewModel=()=> {
     }
 
     const register= async ()=>{
-      const apiResponse= await RegisterAuthUseCase(values)
-      console.log('Result: '+ JSON.stringify(apiResponse));
-      
+      if(isValidForm()){
+        const apiResponse= await RegisterAuthUseCase(values)
+        console.log('Result: '+ JSON.stringify(apiResponse));
+      } 
     }
+
+    const isValidForm = (): boolean =>{
+      if(values.name===''){
+        setErrorMessage('Ingresar tu nombre');
+        return false;
+      }
+      if(values.lastname===''){
+        setErrorMessage('Ingresar apellidos');
+        return false;
+      }
+      if(values.email===''){
+        setErrorMessage('Ingresar email');
+        return false;
+      }
+      if(values.phone===''){
+        setErrorMessage('Ingresar cel/telf');
+        return false;
+      }
+      if(values.password===''){
+        setErrorMessage('Ingresar contraseña');
+        return false;
+      }
+      if(values.confirmPassword===''){
+        setErrorMessage('Ingresar la confirmacion de la contraseña');
+        return false;
+      }
+      if(values.password!==values.confirmPassword){
+        setErrorMessage('Las contraseñas no coninciden');
+        return false;
+      }
+      return true;
+    }
+
   return {
     ...values,
     onChange,
     register,
+    errorMessage,
   }
 }
 
